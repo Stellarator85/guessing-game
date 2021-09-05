ea2939f2e1c6565d8f4b5f9ab3db4dcefd964232
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-09-05 05:31:20 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,17 +1,1 @@
-class GuessingGame {
-	constructor() { }
-	setRange(min, max) {
-		this.max = max;
-		this.min = min;
-	}
-	guess() {
-		return this.min + Math.ceil((this.max - this.min) / 2);
-	}
-	lower() {
-		this.max = this.guess();
-	}
-	greater() {
-		this.min = this.guess();
-	}
-}
-module.exports = GuessingGame;
+
